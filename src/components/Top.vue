726bfac22a38b193ad74290d8ad951c75f26aece
bifree <bifree2bfree@gmail.com>
2021-09-23 14:44:12 +0900

    view編集

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,7 +1,22 @@
 <template>
 	<v-container>
-		<v-card>
-			card
+		<v-card
+			class="my-10"
+			elevation="10"
+			rounded="xl"
+			width="100%"
+		>
+			<v-container class="mx-10 text-center">
+				<v-form>
+					<v-text-field 
+						label="YouTube動画ID"
+						v-model="movieUrl"
+						placeholder="動画URLの【v=】の後に続く英数字"
+						style="width: 300px"
+					/>
+					{{ movieUrl }}
+				</v-form>
+			</v-container>
 		</v-card>
 	</v-container>
 </template>
