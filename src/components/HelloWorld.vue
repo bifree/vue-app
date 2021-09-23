6c1f3ea032df6e49575063d4e355486809f15bdc
bifree <bifree2bfree@gmail.com>
2021-09-23 14:35:04 +0900

    Top.vue add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,8 +16,7 @@
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
+          こんにちは
           <a
             href="https://community.vuetifyjs.com"
             target="_blank"
---
+++ src/components/Top.vue
@@ -0,0 +1,13 @@
+<template>
+	<v-container>
+		<v-card>
+			card
+		</v-card>
+	</v-container>
+</template>
+
+<script>
+
+export default {
+}
+</script>
